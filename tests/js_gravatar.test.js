035c51d7aabb4e03c00735bd5f509a0e65d78d61
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-05-02 11:28:57 +0100

    update to working. Add tests

--- tests/js_gravatar.test.js
+++ tests/js_gravatar.test.js
@@ -1,5 +1,39 @@
+import jsGravatar, { buildQueryStringFromOptions, validateOptions } from '../lib/js-gravatar';
+import { Types, Errors } from '../lib/types';
+
 describe('JS Gravatar', function () {
-  it('1 + 1 should be 2', () => {
-    expect(1 + 1).toEqual(2);
+  describe('Throws an error', function () {
+    it('for missing email and md5hash', function () {
+      expect(() => validateOptions({ defaultImage: '404' })).toThrow(Errors.MISSING_ARGUMENT_ERROR('email or md5Hash'));
+    });
+
+    it('for invalid size options.', function () {
+      expect(() => validateOptions({ email: 'user@email.com', size: 0 })).toThrow(Errors.IMAGE_SIZE_ERROR);
+      expect(() => validateOptions({ email: 'user@email.com', size: 2049 })).toThrow(Errors.IMAGE_SIZE_ERROR);
+    });
+
+    it('for unexpected default image.', function () {
+      expect(() => validateOptions({ email: 'user@email.com', defaultImage: 'not-available' })).toThrow(
+        Errors.DEFAULT_IMAGE_ERROR,
+      );
+    });
+  });
+
+  describe('Correctly generates url query string', function() {
+    const size = 200;
+    const defaultImage = '404';
+
+    it('when only one property is supplied.', function () {
+      expect(buildQueryStringFromOptions({ size })).toBe(`?s=${size}`);
+      expect(buildQueryStringFromOptions({ defaultImage })).toBe(`?d=${defaultImage}`);
+    });
+
+    it('when both size and defaultImage are supplied.', function () {
+      expect(buildQueryStringFromOptions({ defaultImage, size })).toBe(`?s=${size}&d=${defaultImage}`);
+    });
+
+    it('when no property is available.', function () {
+      expect(buildQueryStringFromOptions({})).toBe('');
+    });
   });
 });
